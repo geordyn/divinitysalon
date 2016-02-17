0056edd815b1f5cf605928c8d3e9c20c576b8748
Geordyn Ader <Geordyn Ader>
2016-02-17 14:33:31 -0700

    auth

--- public/app.js
+++ public/app.js
@@ -40,154 +40,154 @@ angular.module('app', ['ui.router'])
   .state('timecard', {
     url: '/timecardAdmin',
     templateUrl: '/views/admin/timecardAdmin.html',
-    controller: 'teamAdminCtrl'
-    // resolve: {
-    //   user: function($state, loginService) {
-    //     return loginService.getCurrentUser()
-    //       .then(function(res) {
-    //         if (res.status != 200) {
-    //           console.log('brack');
-    //           alert('Unauthorized');
-    //           $state.go('home');
-    //
-    //         } else {
-    //           return res.data;
-    //         }
-    //       }, function(err) {
-    //         console.log('brack');
-    //         alert('Unauthorized');
-    //         $state.go('home');
-    //
-    //       });
-    //   }
-    // }
+    controller: 'teamAdminCtrl',
+    resolve: {
+      user: function($state, loginService) {
+        return loginService.getCurrentUser()
+          .then(function(res) {
+            if (res.status != 200) {
+              console.log('brack');
+              alert('Unauthorized');
+              $state.go('home');
+
+            } else {
+              return res.data;
+            }
+          }, function(err) {
+            console.log('brack');
+            alert('Unauthorized');
+            $state.go('home');
+
+          });
+      }
+    }
   })
   .state('scheduleAdmin', {
     url: '/scheduleAdmin',
     templateUrl: '/views/admin/scheduleAdmin.html',
-    controller: 'scheduleAdminCtrl'
-    // resolve: {
-    //   user: function($state, loginService) {
-    //     return loginService.getCurrentUser()
-    //       .then(function(res) {
-    //         if (res.status != 200) {
-    //           console.log('brack');
-    //           alert('Unauthorized');
-    //           $state.go('home');
-    //
-    //         } else {
-    //           return res.data;
-    //         }
-    //       }, function(err) {
-    //         console.log('brack');
-    //         alert('Unauthorized');
-    //         $state.go('home');
-    //
-    //       });
-    //   }
-    // }
+    controller: 'scheduleAdminCtrl',
+    resolve: {
+      user: function($state, loginService) {
+        return loginService.getCurrentUser()
+          .then(function(res) {
+            if (res.status != 200) {
+              console.log('brack');
+              alert('Unauthorized');
+              $state.go('home');
+
+            } else {
+              return res.data;
+            }
+          }, function(err) {
+            console.log('brack');
+            alert('Unauthorized');
+            $state.go('home');
+
+          });
+      }
+    }
   })
   .state('overviewAdmin', {
     url: '/overviewAdmin',
     templateUrl: '/views/admin/overviewAdmin.html',
-    controller: 'overviewAdminCtrl'
-    // resolve: {
-    //   user: function($state, loginService) {
-    //     return loginService.getCurrentUser()
-    //       .then(function(res) {
-    //         if (res.status != 200) {
-    //           console.log('brack');
-    //           alert('Unauthorized');
-    //           $state.go('home');
-    //
-    //         } else {
-    //           return res.data;
-    //         }
-    //       }, function(err) {
-    //         console.log('brack');
-    //         alert('Unauthorized');
-    //         $state.go('home');
-    //
-    //       });
-    //   }
-    // }
+    controller: 'overviewAdminCtrl',
+    resolve: {
+      user: function($state, loginService) {
+        return loginService.getCurrentUser()
+          .then(function(res) {
+            if (res.status != 200) {
+              console.log('brack');
+              alert('Unauthorized');
+              $state.go('home');
+
+            } else {
+              return res.data;
+            }
+          }, function(err) {
+            console.log('brack');
+            alert('Unauthorized');
+            $state.go('home');
+
+          });
+      }
+    }
   })
   .state('clientAdmin', {
     url: '/clientAdmin',
     templateUrl: '/views/admin/clientAdmin.html',
     controller: 'clientCtrl',
-    // resolve: {
-    //   user: function($state, loginService) {
-    //     return loginService.getCurrentUser()
-    //       .then(function(res) {
-    //         if (res.status != 200) {
-    //           console.log('brack');
-    //           alert('Unauthorized');
-    //           $state.go('home');
-    //
-    //         } else {
-    //           return res.data;
-    //         }
-    //       }, function(err) {
-    //         console.log('brack');
-    //         alert('Unauthorized');
-    //         $state.go('home');
-    //
-    //       });
-    //   }
-    // }
+    resolve: {
+      user: function($state, loginService) {
+        return loginService.getCurrentUser()
+          .then(function(res) {
+            if (res.status != 200) {
+              console.log('brack');
+              alert('Unauthorized');
+              $state.go('home');
+
+            } else {
+              return res.data;
+            }
+          }, function(err) {
+            console.log('brack');
+            alert('Unauthorized');
+            $state.go('home');
+
+          });
+      }
+    }
   })
 
   .state('productAdmin', {
     url: '/productAdmin',
     templateUrl: '/views/admin/productAdmin.html',
     controller: 'productAdminCtrl',
-    // resolve: {
-    //   user: function($state, loginService) {
-    //     return loginService.getCurrentUser()
-    //       .then(function(res) {
-    //         if (res.status != 200) {
-    //           console.log('brack');
-    //           alert('Unauthorized');
-    //           $state.go('home');
-    //
-    //         } else {
-    //           return res.data;
-    //         }
-    //       }, function(err) {
-    //         console.log('brack');
-    //         alert('Unauthorized');
-    //         $state.go('home');
-    //
-    //       });
-    //   }
-    // }
+    resolve: {
+      user: function($state, loginService) {
+        return loginService.getCurrentUser()
+          .then(function(res) {
+            if (res.status != 200) {
+              console.log('brack');
+              alert('Unauthorized');
+              $state.go('home');
+
+            } else {
+              return res.data;
+            }
+          }, function(err) {
+            console.log('brack');
+            alert('Unauthorized');
+            $state.go('home');
+
+          });
+      }
+    }
   })
 
   .state('teamAdmin', {
     url: '/teamAdmin',
     templateUrl: '/views/admin/teamAdmin.html',
     controller: 'teamAdminCtrl',
-    // resolve: {
-    //   user: function($state, loginService) {
-    //     return loginService.getCurrentUser()
-    //       .then(function(res) {
-    //         if (res.status != 200) {
-    //           console.log('brack');
-    //           alert('Unauthorized');
-    //           $state.go('home');
-    //
-    //         } else {
-    //           return res.data;
-    //         }
-    //       }, function(err) {
-    //         console.log('brack');
-    //         alert('Unauthorized');
-    //         $state.go('home');
-    //
-    //       });
-    //   }
-    // }
+    resolve: {
+      user: function($state, loginService) {
+        return loginService.getCurrentUser()
+          .then(function(res) {
+            if (res.status != 200) {
+              console.log('brack');
+              alert('Unauthorized');
+              $state.go('home');
+
+            } else {
+              return res.data;
+            }
+          }, function(err) {
+            console.log('brack');
+            alert('Unauthorized');
+            $state.go('home');
+
+          });
+      }
+    }
   });
   $urlRouterProvider.otherwise('/home');
 
